f8356348ab751e7865a2759a42d404286c442aeb
Rodrigo Silvério dos Santos <125663051+RodrigoS908@users.noreply.github.com>
2023-08-17 21:50:58 -0300

    calculadora em phyton

---
+++ calculadora_phyton.py
@@ -0,0 +1,12 @@
+x = float (input("escolha um numero"))
+y = float (input("escolha um numero"))
+z = float (input("1 x 2 - 3 + 4 /"))
+
+if z == 1 :
+    print(x*y)
+elif z == 2 :
+    print(x-y)
+elif z == 3 :
+    print(x+y)
+elif z == 4 :
+    print(x/y)
